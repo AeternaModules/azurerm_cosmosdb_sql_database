08fb7b4fed06e60770ac867cd2323a080889c9b1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "cosmosdb_sql_databases" {
-  description = "All cosmosdb_sql_database resources"
-  value       = azurerm_cosmosdb_sql_database.cosmosdb_sql_databases
-}
 output "cosmosdb_sql_databases_account_name" {
-  description = "List of account_name values across all cosmosdb_sql_databases"
-  value       = [for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : v.account_name]
+  description = "Map of account_name values across all cosmosdb_sql_databases, keyed the same as var.cosmosdb_sql_databases"
+  value       = { for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : k => v.account_name }
 }
 output "cosmosdb_sql_databases_autoscale_settings" {
-  description = "List of autoscale_settings values across all cosmosdb_sql_databases"
-  value       = [for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : v.autoscale_settings]
+  description = "Map of autoscale_settings values across all cosmosdb_sql_databases, keyed the same as var.cosmosdb_sql_databases"
+  value       = { for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : k => v.autoscale_settings }
 }
 output "cosmosdb_sql_databases_name" {
-  description = "List of name values across all cosmosdb_sql_databases"
-  value       = [for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : v.name]
+  description = "Map of name values across all cosmosdb_sql_databases, keyed the same as var.cosmosdb_sql_databases"
+  value       = { for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : k => v.name }
 }
 output "cosmosdb_sql_databases_resource_group_name" {
-  description = "List of resource_group_name values across all cosmosdb_sql_databases"
-  value       = [for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : v.resource_group_name]
+  description = "Map of resource_group_name values across all cosmosdb_sql_databases, keyed the same as var.cosmosdb_sql_databases"
+  value       = { for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : k => v.resource_group_name }
 }
 output "cosmosdb_sql_databases_throughput" {
-  description = "List of throughput values across all cosmosdb_sql_databases"
-  value       = [for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : v.throughput]
+  description = "Map of throughput values across all cosmosdb_sql_databases, keyed the same as var.cosmosdb_sql_databases"
+  value       = { for k, v in azurerm_cosmosdb_sql_database.cosmosdb_sql_databases : k => v.throughput }
 }
 
